443edb87b219c590ee1c53ef6714d71568d8db98
xandejpeg <xande.chiareli@gmail.com>
2024-06-21 21:15:51 +0700

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+// Servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'assets')));
+
+// Rota para o index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para o inner-page.html
+app.get('/inner-page.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'inner-page.html'));
+});
+
+// Rota para o portfolio-details.html
+app.get('/portfolio-details.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'portfolio-details.html'));
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
